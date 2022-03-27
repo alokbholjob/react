2d920789220e43bc28f04edd3c9ba3cfa0d8494e
Alok Kumar Bhol <kalokbhol77@gmail.com>
2022-03-27 18:19:41 +0530

    Hook- depedency array explained/ why do we need that

--- Home.jsx
+++ Home.jsx
@@ -8,19 +8,25 @@ const Home = () => {
     {title: "Web dev tips", body: "lorem ipsum....", author: "Mario", id:3},
   ]);
 
+  const [name, setName] = useState("Mario");
+
   const handleDelete = (id) =>{
     const newBlogs = blogs.filter((blog)=> blog.id !== id ); //newBlogs will store the filtered data
     setBlogs(newBlogs);
   } 
 
+  /* The second argument [] of the useEffect hook controls when the next function should run. 
+  If we remove the second argument(depedency array) this hook will re-render the function */ 
   useEffect(()=>{
     console.log("useEffect ran");
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className='home'>
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
       <BlogList blogs={blogs.filter( (blog)=> blog.author === 'Mario' ) } title="Mario's Blog" />
+      <button onClick={()=>setName("Luigie") } > Change Name  </button>
     </div>
   )
 }
