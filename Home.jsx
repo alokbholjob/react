588f51fc706638d8a373245d1fcaed251e2ad530
Alok Kumar Bhol <kalokbhol77@gmail.com>
2022-03-29 19:54:40 +0530

    conditional loading messages

--- Home.jsx
+++ Home.jsx
@@ -3,45 +3,35 @@ import BlogList from './BlogList';
 
 
 /* use this command to run the json data-- npx json-server --watch folder_name/file_name --port port_number */
+/* The second argument [] of the useEffect hook controls when the next function should run. 
+If we remove the second argument(depedency array) this hook will re-render the function */ 
 
 const Home = () => {
-  // const [blogs, setBlogs] = useState([
-  //   {
-  //     title: "My first Blog",
-  //     body: "This is a react tutorial instructed by youtuber The NetNinja",
-  //     author: "Mario",
-  //     id: 1
-  //   },
-  //   {
-  //     title: "Youtube learning",
-  //     body: "Learn the concepts, be technically strong",
-  //     author: "Yoshi",
-  //     id: 2
-  //   }
-  // ]);
-
-const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState("Mario");
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleDelete = (id) =>{
     const newBlogs = blogs.filter((blog)=> blog.id !== id ); //newBlogs will store the filtered data
     setBlogs(newBlogs);
   } 
 
-  /* The second argument [] of the useEffect hook controls when the next function should run. 
-  If we remove the second argument(depedency array) this hook will re-render the function */ 
   useEffect(()=>{
-    fetch('http://localhost:8000/blogs')
-    .then(res =>{
-      return res.json()
-    })
-    .then(data =>{
-      setBlogs(data);
-    })
+    setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
+      .then(res =>{
+        return res.json()
+      })
+      .then(data =>{
+        setBlogs(data);
+        setIsLoading(false);
+      })
+    },(1000));    
   }, []);
 
   return (
     <div className='home'>
+      {isLoading && <div>Loading...</div>}
     {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
 
       {/* <BlogList blogs={blogs.filter( (blog)=> blog.author === 'Mario' ) } title="Mario's Blog" /> */}
@@ -49,5 +39,4 @@ const [blogs, setBlogs] = useState(null);
     </div>
   )
 }
-
 export default Home;
