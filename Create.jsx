2223b049ba6bb5d2ab636d2734785a4f47d6ef17
Alok Kumar Bhol <kalokbhol77@gmail.com>
2022-03-31 12:47:19 +0530

    Tut-27 Controlled Inputs(FORMS)

--- Create.jsx
+++ Create.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+    const [title, setTitle] = useState('hello');
+    const [body, setBody] = useState();
+    const [authorName, setAuthorName] = useState('Mario');
+
   return (
     <div className='create'>
         
-     <h1>Add a new blog</h1>
+     <h2>Add a new blog</h2>
+     <form>
+         <label>Blog Title: </label>
+         <input
+            type="text"
+            required
+            value={title}
+            onChange={(e)=> setTitle(e.target.value)} //two way binding
+         />
+         <label>Blog Body: </label>
+         <textarea 
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+        >
+         </textarea>
+        
+         <label>Blog Author: </label>
+         <select 
+            required
+            value={authorName}
+            onChange={(e)=> setAuthorName(e.target.value)}
+         >
+             <option value="Mario"> Mario </option>
+             <option value="Ashis"> Ashis </option>
+         </select>
+         <button>Add Blog</button>
+
+         <p> {title} </p>
+         <p> {body} </p>
+         <p> {authorName} </p>
+     </form>
      </div>
   )
 }
