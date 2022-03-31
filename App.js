b50a348755ec16233d39ebe55291d308fe57e210
Alok Kumar Bhol <kalokbhol77@gmail.com>
2022-03-31 10:31:18 +0530

    Tut-25 Route parameters

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -14,11 +15,14 @@ function App() {
 
         <div className="content">
           <Switch>
-            <Route exact path='/' >
+            <Route exact path='/'>
             <Home />
             </Route>
-            <Route path='/create' >
+            <Route path='/create'>
             <Create />
+            </Route>
+            <Route path='/blogs/:id'>
+            <BlogDetails />
             </Route>
           </Switch>
         </div>
--- BlogList.jsx
+++ BlogList.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const BlogList = ({ blogs,title, handleDelete }) =>{
     //another way by destructuring--> (props)/ ({ title, author })
     //const blogs = props.blogs;
     //const title = props.title;
-    const author = blogs.author;
 
     return(
         <div className="blog-list">
             <h2>{title} </h2>
                { blogs.map( (blog)=>(
           <div className="blog-preview" key={blog.id}>
-            <h2>{title}</h2>
-            <p>Written by {author}</p>
+            <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
             {/* <button onClick={()=> handleDelete(blog.id)} >Delete Blog</button> */}
+            </Link>
           </div>
       ))}
         </div>
